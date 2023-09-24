d1a300d442c6cee2bbb568c8140c5971b4a027a8
ckit0606 <143493105+ckit0606@users.noreply.github.com>
2023-09-24 14:16:59 +0800

    app.py

--- app.py
+++ app.py
@@ -132,4 +132,7 @@ def jobDetail(jobID=None):
     cursor.close()
     return render_template("jobDetail.html",jobData=jobData)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+
 
